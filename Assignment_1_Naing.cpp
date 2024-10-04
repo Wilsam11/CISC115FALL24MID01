5618b16def881aacf98f2e332cda90c343146201
Wilsam11 <myosatnaing144@gmail.com>
2024-10-04 02:41:39 +0000

    First Assignment

---
+++ Assignment_1_Naing.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //  variables
+    int age = 12;
+    float height = 5.8;
+    char grade = 'B';
+    string Fullname = "Myo Set Naing";
+    
+    //  a basic operation
+    age = age + 10;
+    
+    // Output the results
+    cout << "Fullname: " << Fullname << endl;
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << endl;
+    cout << "Grade: " << grade << endl;
+    
+    // Implicit conversion
+    float newAge = age;
+    cout << "New Age as float: " << newAge << endl;
+
+    // Explicit conversion
+    int heightAsInt = static_cast<int>(height);
+    cout << "Height as int: " << heightAsInt << endl;
+
+    return 0;
+}
